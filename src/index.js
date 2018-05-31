f4ecf40fd4896d016bd5571615655ae949543276
dayfine <difancpa@gmail.com>
2018-05-30 20:40:59 -0400

    remove unneeded files and dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const
   MineSweeperGame = require('./Game'),
-  minimist = require('minimist'),
-  inquirer = require('inquirer'),
   GUI = require('./GUI')
-
-const argv = minimist(process.argv.slice(2))
-console.log('argvs:', argv)
 
 const game = new MineSweeperGame()
 game.init()
